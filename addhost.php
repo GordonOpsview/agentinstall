afb2a766c0943f8574dff0125942423676499665
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2024-09-12 15:03:41 +0100

    3rd one worked

--- addhost.php
+++ addhost.php
@@ -7,8 +7,7 @@ $hostname = $_GET['hostname'];
 $hostip = $_GET['hostip'];
 
 // Execute the bash script with parameters
-exec("bash $bashScript $hostname $hostip || exit \$?", $output, $returnVar);
-exec("echo asdf >> asdf.log", $outputasdf, $returnVarasdf);
+exec("bash $bashScript $hostname $hostip", $output, $returnVar);
 // Check if the script executed successfully
 if ($returnVar != 0) {
     header('HTTP/1.1 500 Internal Server Error');
