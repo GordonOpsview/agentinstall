d2cf9070557071529287a456f4d8b1de18587c76
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2024-09-18 10:49:39 +0100

    Fixed the opsview_rest quoting problem.

--- addhost.sh
+++ addhost.sh
@@ -6,6 +6,7 @@ configopt () {
 OPSVIEW_USER=$(configopt 'user')
 OPSVIEW_PASSWORD=$(configopt 'password')
 
+tokenfile=./.token
 hostname=$1
 hostip=$2
 daemons="$(echo $3 | base64 -d | gzip -d)"
@@ -21,13 +22,12 @@ for d in $daemons; do
 done
 
 for t in "${templates[@]}"; do
-  tlist=$tlist',{"name": "'$t'" }'
+  tlist="$tlist,{\"name\": \"$t\" }"
 done
 
-/opt/opsview/coreutils/bin/opsview_rest --username $OPSVIEW_USER --password $OPSVIEW_PASSWORD --token-file ./.token GET info &>/dev/null
-/opt/opsview/coreutils/bin/opsview_rest --token-file ./.token --data-format=json --data='{
-  "name": "'$hostname'",
-  "ip": "'$hostip'",
-  "hosttemplates": [ '${tlist#,}' ]
-}' PUT config/host
+if (($(date +%s)-$(date -r $tokenfile +%s)>900)); then 
+  # Only login if the token is older than 15min
+  /opt/opsview/coreutils/bin/opsview_rest --username=$OPSVIEW_USER --password=$OPSVIEW_PASSWORD --token-file $tokenfile GET info &>/dev/null
+fi
+/opt/opsview/coreutils/bin/opsview_rest --token-file $tokenfile --data-format=json --data="{\"name\": \"$hostname\", \"ip\": \"$hostip\", \"hosttemplates\": [ ${tlist#,} ]}" PUT config/host
 # /opt/opsview/coreutils/bin/rc.opsview gen_config
